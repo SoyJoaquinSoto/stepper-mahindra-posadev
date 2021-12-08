013655ab4ae02ad81594d3e630bde39cc3ed9542
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-12-07 23:12:00 -0600

    Creación del header
     - Se agregó el logo de Tech Mahindra
     - Se agregó el título del desafío
     - Se hicieron los estilos acorde al diseño

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from "./layout/Header";
+
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/layout/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+	return (
+		<div className="flex flex-row items-end justify-start gap-20 p-4">
+			<img
+				className="w-1/5"
+				src="mahindra.png"
+				alt="Logo de Tech Mahindra; 'Tech' en fuente gris en la parte superior y 'Mahindra' en la inferior con fuente roja"
+			/>
+			<h1 className="text-rojo_mihandra font-bold text-2xl pb-2">
+				Stepper - Frontend Coding Challenge
+			</h1>
+		</div>
+	);
+}
+
+export default Header;
