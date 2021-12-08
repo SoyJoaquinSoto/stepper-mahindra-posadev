c1e8b752f84f8ef4778be6626eaf84b7e3f47a74
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-12-07 23:45:19 -0600

    Se obtiene el contenido a mostrar de forma dinámica para cada paso

--- src/components/Step.js
+++ src/components/Step.js
@@ -1,8 +1,16 @@
 import { useParams } from "react-router-dom";
+import steps from "../steps";
 
 const Step = () => {
-	let { id } = useParams();
-	return <div>{id}</div>;
+	const { id } = useParams();
+	const contenido_encontrado = steps.find((step) => step.id === Number(id));
+	let contenido = "Step not found!";
+
+	if (contenido_encontrado) {
+		contenido = contenido_encontrado.content;
+	}
+
+	return <div>{contenido}</div>;
 };
 
 export default Step;
